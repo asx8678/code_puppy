d27a32719603dc6a60d5f0295b06c2825e242112
asx8678 <114666958+asx8678@users.noreply.github.com>
2026-04-08 15:11:14 +0100

    feat(utils): consolidate fallback emit_* functions into shared module
    - Create new shared module at code_puppy/utils/emit.py
    - Move fallback emit_error, emit_info, emit_success, emit_warning functions
    - Update utils/__init__.py to export emit functions
    - Update pop_command/register_callbacks.py to import from shared module
    - Remove duplicate emit_* function definitions from pop_command
    
    Closes code_puppy-snqw

--- code_puppy/utils/__init__.py
+++ code_puppy/utils/__init__.py
@@ -3,6 +3,7 @@
 Inspired by patterns from oh-my-pi (omp) project.
 """
 
+from .emit import emit_error, emit_info, emit_success, emit_warning
 from .ring_buffer import RingBuffer
 from .parallel import map_with_concurrency, Semaphore, ParallelResult
 from .dag import build_dependency_graph, detect_cycles, build_execution_waves
@@ -10,6 +11,12 @@ from .shell_split import split_compound_command
 from .stream_parser import StreamLineParser, SSEParser, parse_jsonl_lenient
 
 __all__ = [
+    # Emit utilities
+    "emit_error",
+    "emit_info",
+    "emit_success",
+    "emit_warning",
+    # Existing utilities
     "RingBuffer",
     "map_with_concurrency",
     "Semaphore",
