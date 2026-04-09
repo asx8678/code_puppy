52705c641849ea463f0aec0f9e21310e0d062384
asx8678 <114666958+asx8678@users.noreply.github.com>
2026-04-09 07:21:00 +0100

    chore: sync working tree snapshot to fork
    Includes Rust build artifact updates under target/debug/ and
    untracked coverage pid files from recent test runs.
    
    Pushed to asx8678/code_puppy fork.

--- target/debug/deps/_code_puppy_core-5021a2858c146de7.d
+++ target/debug/deps/_code_puppy_core-5021a2858c146de7.d
@@ -1,6 +1,6 @@
-/Users/adam2/projects/bd-7m88/target/debug/deps/_code_puppy_core-5021a2858c146de7.d: code_puppy_core/src/lib.rs code_puppy_core/src/hashline.rs code_puppy_core/src/message_hashing.rs code_puppy_core/src/pruning.rs code_puppy_core/src/serialization.rs code_puppy_core/src/token_estimation.rs code_puppy_core/src/types.rs
+/Users/adam2/projects/code_puppy/target/debug/deps/_code_puppy_core-5021a2858c146de7.d: code_puppy_core/src/lib.rs code_puppy_core/src/hashline.rs code_puppy_core/src/message_hashing.rs code_puppy_core/src/pruning.rs code_puppy_core/src/serialization.rs code_puppy_core/src/token_estimation.rs code_puppy_core/src/types.rs
 
-/Users/adam2/projects/bd-7m88/target/debug/deps/lib_code_puppy_core-5021a2858c146de7.rmeta: code_puppy_core/src/lib.rs code_puppy_core/src/hashline.rs code_puppy_core/src/message_hashing.rs code_puppy_core/src/pruning.rs code_puppy_core/src/serialization.rs code_puppy_core/src/token_estimation.rs code_puppy_core/src/types.rs
+/Users/adam2/projects/code_puppy/target/debug/deps/lib_code_puppy_core-5021a2858c146de7.rmeta: code_puppy_core/src/lib.rs code_puppy_core/src/hashline.rs code_puppy_core/src/message_hashing.rs code_puppy_core/src/pruning.rs code_puppy_core/src/serialization.rs code_puppy_core/src/token_estimation.rs code_puppy_core/src/types.rs
 
 code_puppy_core/src/lib.rs:
 code_puppy_core/src/hashline.rs:
--- target/debug/deps/turbo_ops-7e135bd55076b3bf.d
+++ target/debug/deps/turbo_ops-7e135bd55076b3bf.d
@@ -1,6 +1,6 @@
-/Users/adam2/projects/bd-7m88/target/debug/deps/turbo_ops-7e135bd55076b3bf.d: turbo_ops/src/lib.rs turbo_ops/src/batch_executor.rs turbo_ops/src/models.rs turbo_ops/src/operations.rs
+/Users/adam2/projects/code_puppy/target/debug/deps/turbo_ops-7e135bd55076b3bf.d: turbo_ops/src/lib.rs turbo_ops/src/batch_executor.rs turbo_ops/src/models.rs turbo_ops/src/operations.rs
 
-/Users/adam2/projects/bd-7m88/target/debug/deps/libturbo_ops-7e135bd55076b3bf.rmeta: turbo_ops/src/lib.rs turbo_ops/src/batch_executor.rs turbo_ops/src/models.rs turbo_ops/src/operations.rs
+/Users/adam2/projects/code_puppy/target/debug/deps/libturbo_ops-7e135bd55076b3bf.rmeta: turbo_ops/src/lib.rs turbo_ops/src/batch_executor.rs turbo_ops/src/models.rs turbo_ops/src/operations.rs
 
 turbo_ops/src/lib.rs:
 turbo_ops/src/batch_executor.rs:
