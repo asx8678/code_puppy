11763b0ebc7ff50a5b02f6e3c71f402a96ce2f70
asx8678 <114666958+asx8678@users.noreply.github.com>
2026-04-17 14:09:46 +0100

    feat(bd-115): Delete Rust parsing code - pure Elixir now

--- turbo_parse/queries/elixir/folds.scm
+++
@@ -1,9 +0,0 @@
-;;; Elixir Folds Query
-;;; Source: Helix Editor (https://github.com/helix-editor/helix)
-;;; License: MPL-2.0 (Helix Editor queries are MIT compatible)
-
-[
-  (call)
-  (do_block)
-  (stab_clause)
-] @fold
